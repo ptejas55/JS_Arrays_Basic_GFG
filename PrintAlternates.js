9bc9cf8133e9978ce81c184f83139b2ca3d5bf49
Tejas <ptejas123456789@gmail.com>
2025-06-26 00:40:41 +0530

    filter method

--- PrintAlternates.js
+++ PrintAlternates.js
@@ -1,6 +1,12 @@
-const array= [10,20,30,40,50]
-const res=[]
-for(let i=0;i<=array.length;i=i+2){
-    res.push(array[i])
+const array = [10, 20, 30, 40, 50];
+
+//using filter method
+const res = array.filter((_, index) => index % 2 === 0);
+console.log(res);
+
+//using for loop
+const res1 = [];
+for (let i = 0; i <= array.length; i = i + 2) {
+  res.push(array[i]);
 }
-console.log(res)
+console.log(res1);
